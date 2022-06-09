76f351c3decbd4b69b9dc7b2cb8e2e2925485f7d
Sharikekkk <skvorcovi662@gmail.com>
2022-06-09 16:21:54 +0400

    Начало разработки программы

--- ConsoleAppTests/Program.cs
+++ ConsoleAppTests/Program.cs
@@ -6,7 +6,27 @@ namespace ConsoleAppTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Введите 1, если хотите создать тест,  или введите 2, если хотите пройти тест");
+            try 
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (a == 1)
+                {
+
+                }
+                else if (a == 2)
+                {
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Вы ввели некорректное значение");
+            }
         }
     }
 }
