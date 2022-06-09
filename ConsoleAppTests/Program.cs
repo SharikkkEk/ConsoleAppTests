bbfa09d6e4a9f7d4515e146d7bc143984a2ccfe3
Sharikekkk <skvorcovi662@gmail.com>
2022-06-09 20:59:20 +0400

    Добавил две неполные функции

--- ConsoleAppTests/Program.cs
+++ ConsoleAppTests/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace ConsoleAppTests
 {
+    struct test
+    {
+        int test_count_questions;
+    }
     class Program
     {
+        public static List<string> Test = new List<string>();
         static void Main(string[] args)
         {
             Menu();
@@ -16,17 +23,29 @@ namespace ConsoleAppTests
                 int a = Convert.ToInt32(Console.ReadLine());
                 if (a == 1)
                 {
-
+                    CreateTest();
                 }
                 else if (a == 2)
                 {
-
+                    PassTest();
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели некорректное значение");
                 }
             }
             catch
             {
                 Console.WriteLine("Вы ввели некорректное значение");
             }
+        }
+        static void CreateTest()
+        {
+
+        }
+        static void PassTest()
+        {
+
         }
     }
 }
---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Test
+{
+    struct book
+    {
+        public string title;
+        public string name;
+        public string author;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            book b;
+            b.author = "King";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
